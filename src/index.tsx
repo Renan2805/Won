de0d2e1d1acf05a96cfb922d5a69e8ae9adea18c
renan2805 <renansilva.dev@gmail.com>
2022-04-22 21:19:07 -0300

    Atualizações

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = createRoot(container)
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/WonTI" element={<App />} />
       <Route path="*" element={<NotFoundPage />}>
       </Route>
     </Routes>
